ec223f419c2b5b4cbbb60ac4bbda7ecacb9da5e1
cristinavn <venanar@hotmail.com>
2018-03-25 16:27:56 +0200

    Deleted warnings

--- src/main/java/com/uniovi/controllers/FriendsController.java
+++ src/main/java/com/uniovi/controllers/FriendsController.java
@@ -12,7 +12,6 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 import com.uniovi.entities.User;
 import com.uniovi.services.UsersService;
